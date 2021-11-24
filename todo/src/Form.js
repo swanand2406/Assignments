7110fdcfff23a0a279dfc6334d3cc3a67ac1e4fe
swanand2406 <swanand.sambhus@bluepineapple.io>
2021-11-24 14:47:43 +0530

    change

--- todo/src/Form.js
+++ todo/src/Form.js
@@ -15,6 +15,7 @@ const Form = ({ error, value, submit, onChange }) => {
 						onChange={onChange}
 					/>
 					{error && <small className='form-text text-danger'>{error}</small>}
+					
 				</div>
 				<div className='col-3 text-right'>
 					<button type='submit' className='btn btn-success'>
